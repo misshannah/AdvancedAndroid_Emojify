776dad5233a2a09d32d06b009b72f761183c6c38
hannaholukoye <hannaholukoye@gmail.com>
2018-08-16 12:13:14 +0300

    feat: Add code to detect faces.

---
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,44 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+
+/**
+ * Created by hannaholukoye on 15/08/2018.
+ */
+
+public class Emojifier {
+    public static void detectFaces(Context context, Bitmap bitmap) {
+        // Create the face detector, disable tracking and enable classifications
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        // Build the frame
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+
+        // Detect the faces
+        SparseArray<Face> faces = detector.detect(frame);
+
+        // Log the number of faces
+        Log.d("TAG", "detectFaces: number of faces = " + faces.size());
+
+        // If there are no faces detected, show a Toast message
+        if(faces.size() == 0){
+            Toast.makeText(context, R.string.no_faces_message, Toast.LENGTH_SHORT).show();
+        }
+
+        // Release the detector
+        detector.release();
+    }
+
+
+}
